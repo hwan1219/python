4d1360f50e9300c36b7d0e1fc0a4dc825cd91e6b
hwan <ljh961219@naver.com>
2025-04-28 17:34:01 +0900

    first commit

---
+++ 01_PythonBasic/etc/var02.py
@@ -0,0 +1,15 @@
+a = 42 # int (정수)
+b = 3.14159 # float (실수)
+c = "Hello" # str (문자열)
+d = True # boll (불리언: true or false)
+
+# 각 데이터 타입 출력
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
